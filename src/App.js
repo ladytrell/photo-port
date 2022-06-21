ded7cf997b6f628b987c44a638af2e6ee9a0de8b
Lady Trell <antrellkent@gmail.com>
2022-06-21 08:39:25 -0400

    about and nav components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-     <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <main>
+        <Nav></Nav>
+        <About></About>
+      </main>
     </div>
   );
 }
